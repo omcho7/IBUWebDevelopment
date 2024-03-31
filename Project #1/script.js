67dd4246fe087b8b17cd5483665b154ce3f2e3c1
omcho7 <osmanovicomar@gmail.com>
2024-03-31 17:11:15 +0200

    Project week 5: Minor aesthetic edits, added form validation, added animations

--- Project #1/script.js
+++ Project #1/script.js
@@ -1,15 +1,15 @@
 window.onload = function() {
-    var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+    var myModal = new bootstrap.Modal(document.getElementById('cookieModal'));
     myModal.show();
   }
 
   // Close the modal when either button is clicked
   document.getElementById('option1').addEventListener('click', function() {
-    var myModal = bootstrap.Modal.getInstance(document.getElementById('exampleModal'));
+    var myModal = bootstrap.Modal.getInstance(document.getElementById('cookieModal'));
     myModal.hide();
   });
 
   document.getElementById('option2').addEventListener('click', function() {
-    var myModal = bootstrap.Modal.getInstance(document.getElementById('exampleModal'));
+    var myModal = bootstrap.Modal.getInstance(document.getElementById('cookieModal'));
     myModal.hide();
   });
