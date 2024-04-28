f539ec2e4d759d3211501f4c0756168e0368c92c
omcho7 <osmanovicomar@gmail.com>
2024-04-28 21:51:29 +0200

    Project 2 week 1: added button to return user to top of site

--- Project #1/script.js
+++ Project #1/script.js
@@ -19,3 +19,27 @@ window.onload = function() {
     var myModal = bootstrap.Modal.getInstance(document.getElementById('cookieModal'));
     myModal.hide();
   });
+
+
+
+  // Get the button for scroll to top
+var scrollToTopButton = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopButton.style.display = "block";
+    } else {
+        scrollToTopButton.style.display = "none";
+    }
+}
+
+// scroll to the top of the document
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
